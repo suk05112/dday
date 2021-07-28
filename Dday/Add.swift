3a22ba37ef49db5db93b2a6e5f24194157a98ebc
한수진 <hansj4525@likelion.org>
2021-07-28 22:23:56 +0900

    first commit

---
+++ Dday/Add.swift
@@ -0,0 +1,29 @@
+//
+//  Add.swift
+//  Dday
+//
+//  Created by 한수진 on 2021/07/22.
+//
+
+import UIKit
+
+class Add: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
